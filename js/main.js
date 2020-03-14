6f1b93a9f8f9d227621f67b95b339d2559f28184
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-03-14 16:43:57 +0500

    Правки

--- js/main.js
+++ js/main.js
@@ -2,34 +2,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
   'use strict';
   
-  // Функция для popUp
-  const popUp = (idPopUp) => {
-    
-    const popUp = document.querySelectorAll('.popup');
+  
 
-    popUp.forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-        let target = event.target;
-        
-        if(!target.closest('.form-content')){
-          idPopUp.style.display = 'none'; 
-        }
-        
-        console.log(target);
-        
+  // Функция появления модального окна
+  const modal = (elem, tar) => {
+    // Функция для popUp
+    const popUp = (idPopUp) => {      
+      const popUp = document.querySelectorAll('.popup');
+
+      popUp.forEach((elem) => {
+        elem.addEventListener('click', (event) => {
+          let target = event.target;
+          
+          if(!target.closest('.form-content') || target.closest('.close-btn')){
+            idPopUp.style.display = 'none'; 
+          } 
+          
+        });
       });
+    };
 
-    });
+    if(tar.closest(elem)){
+      let dataSet = tar.dataset.popup.split('').slice(1).join(''),
+        id = document.getElementById(dataSet);
+
+      id.style.display = 'block';      
+      popUp(id);        
+    }
   };
   
+  // Подарок
+  const gift = () => {
+    // Блок с картинкой
+    const fixedFift = document.querySelector('.fixed-gift');
+
+    fixedFift.addEventListener('click', (event) => {
+      let target = event.target;
+      if(target.closest('.fixed-gift')){
+        fixedFift.style.display = 'none';
+        modal('.fixed-gift', target);        
+      }
+    });
+  };
+  gift();
 
   // Блок Header
   const header = () => {
     const
       // Блок с header
-      headerMain = document.querySelector('.header-main'),
-      // PopUp free_visit_form
-      freeVisitForm = document.getElementById('free_visit_form');
+      headerMain = document.querySelector('.header-main');
 
     headerMain.addEventListener('click', (event) => {
       let target = event.target;
@@ -47,29 +68,31 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       if(target.closest('.clubs-list ul')){
         ul.style.transform = `scaleY(1)`;        
-      }
+      }      
 
       // Записаться на бесплатный приём
-      if(target.closest('.free-visit p')){
-        let dataSet = target.dataset.popup.split('').slice(1).join(''),
-          id = document.getElementById(dataSet);
-
-        id.style.display = 'block';
-        popUp(id);
-        
-      } 
+      modal('.free-visit p', target);
 
       // Перезвоните мне
-      if(target.closest('.callback-btn')){
-        let dataSet = target.dataset.popup.split('').slice(1).join(''),
-          id = document.getElementById(dataSet);
+      modal('.callback-btn', target);
 
-        id.style.display = 'block';
-        popUp(id);  
+      // Бургер меню
+      const menu = document.querySelector('.popup-menu');
+      if(target.closest('.menu-button')){
+        menu.style.display = `flex`;        
+      } else {
+        console.log('no');        
+      }
+      if(target.closest('.close-menu-btn') || target.closest('.scroll')){
+        menu.style.display = `none`;
       }
     });  
   };
   header();
+
+
+
+  
 
   
 
